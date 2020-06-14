568d3a01fee8b7cf47366e106a0db09c6c9aada1
Alex <inkisidor_117@hotmail.com>
2020-06-13 22:12:33 -0500

    Ahora se carga la lista de alumnos en el datagrid

--- Proyecto-Calificaciones/Alta_Asistencias.Designer.cs
+++ Proyecto-Calificaciones/Alta_Asistencias.Designer.cs
@@ -34,12 +34,15 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column6 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewImageColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,7 +51,7 @@
             this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dateTimePicker1.Enabled = false;
-            this.dateTimePicker1.Location = new System.Drawing.Point(1052, 54);
+            this.dateTimePicker1.Location = new System.Drawing.Point(1052, 25);
             this.dateTimePicker1.MaxDate = new System.DateTime(2024, 12, 31, 0, 0, 0, 0);
             this.dateTimePicker1.MinDate = new System.DateTime(2020, 6, 5, 0, 0, 0, 0);
             this.dateTimePicker1.Name = "dateTimePicker1";
@@ -60,7 +63,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(968, 54);
+            this.label2.Location = new System.Drawing.Point(968, 22);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 25);
             this.label2.TabIndex = 21;
@@ -88,9 +91,7 @@
             this.Column1,
             this.Column2,
             this.Column3,
-            this.Column4,
-            this.Column5,
-            this.Column6});
+            this.Column4});
             this.dataGridView1.Location = new System.Drawing.Point(12, 116);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 10;
@@ -105,6 +106,7 @@
             this.button1.TabIndex = 23;
             this.button1.Text = "Seleccionar Todos";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -115,41 +117,111 @@
             this.button2.Text = "Registrar";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1132, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 30);
+            this.button3.TabIndex = 102;
+            this.button3.Text = "Cargar lista";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C"});
+            this.comboBox2.Location = new System.Drawing.Point(1059, 68);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(67, 28);
+            this.comboBox2.TabIndex = 101;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(976, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 25);
+            this.label3.TabIndex = 100;
+            this.label3.Text = "Grupo:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.comboBox1.Location = new System.Drawing.Point(903, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(67, 28);
+            this.comboBox1.TabIndex = 99;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(820, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 25);
+            this.label4.TabIndex = 98;
+            this.label4.Text = "Grado:";
+            // 
             // Column1
             // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Column1.FillWeight = 277.7778F;
             this.Column1.HeaderText = "Seleccionar";
             this.Column1.Name = "Column1";
+            this.Column1.Width = 69;
             // 
             // Column2
             // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.DataPropertyName = "no_control";
+            this.Column2.FillWeight = 53.94965F;
             this.Column2.HeaderText = "Número de control";
             this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
             // 
             // Column3
             // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.DataPropertyName = "nombre";
+            this.Column3.FillWeight = 53.94965F;
             this.Column3.HeaderText = "Nombre";
             this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
             // 
             // Column4
             // 
-            this.Column4.HeaderText = "Grado";
+            this.Column4.DataPropertyName = "foto";
+            this.Column4.FillWeight = 14.32292F;
+            this.Column4.HeaderText = "Fotografía";
+            this.Column4.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
             this.Column4.Name = "Column4";
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "Grupo";
-            this.Column5.Name = "Column5";
-            // 
-            // Column6
-            // 
-            this.Column6.HeaderText = "Fotografía";
-            this.Column6.Name = "Column6";
             // 
             // Alta_Asistencias
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 641);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker1);
@@ -171,13 +243,16 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
-        private System.Windows.Forms.DataGridViewImageColumn Column6;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewImageColumn Column4;
     }
 }
--- Proyecto-Calificaciones/Alta_Asistencias.cs
+++ Proyecto-Calificaciones/Alta_Asistencias.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Proyecto_Calificaciones
 {
@@ -17,10 +18,128 @@ namespace Proyecto_Calificaciones
             InitializeComponent();
         }
 
+        int asignacion = 0;
+        MySqlConnection Conexion = new MySqlConnection();
+
         private void Alta_Asistencias_Load(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
             this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MostrarTabla();
+        }
+
+        private void AsignacionID()
+        {
+            asignacion = 0;
+
+            if (comboBox1.Text == "1" && comboBox2.Text == "A")
+            {
+                asignacion = 2;
+            }
+            else if (comboBox1.Text == "1" && comboBox2.Text == "B")
+            {
+                asignacion = 3;
+            }
+            else if (comboBox1.Text == "1" && comboBox2.Text == "C")
+            {
+                asignacion = 4;
+            }
+            else if (comboBox1.Text == "2" && comboBox2.Text == "A")
+            {
+                asignacion = 5;
+            }
+            else if (comboBox1.Text == "2" && comboBox2.Text == "B")
+            {
+                asignacion = 6;
+            }
+            else if (comboBox1.Text == "2" && comboBox2.Text == "C")
+            {
+                asignacion = 7;
+            }
+            else if (comboBox1.Text == "3" && comboBox2.Text == "A")
+            {
+                asignacion = 8;
+            }
+            else if (comboBox1.Text == "3" && comboBox2.Text == "B")
+            {
+                asignacion = 9;
+            }
+            else if (comboBox1.Text == "3" && comboBox2.Text == "C")
+            {
+                asignacion = 10;
+            }
+            else if (comboBox1.Text == "4" && comboBox2.Text == "A")
+            {
+                asignacion = 11;
+            }
+            else if (comboBox1.Text == "4" && comboBox2.Text == "B")
+            {
+                asignacion = 12;
+            }
+            else if (comboBox1.Text == "4" && comboBox2.Text == "C")
+            {
+                asignacion = 13;
+            }
+            else if (comboBox1.Text == "5" && comboBox2.Text == "A")
+            {
+                asignacion = 14;
+            }
+            else if (comboBox1.Text == "5" && comboBox2.Text == "B")
+            {
+                asignacion = 15;
+            }
+            else if (comboBox1.Text == "5" && comboBox2.Text == "C")
+            {
+                asignacion = 16;
+            }
+            else if (comboBox1.Text == "6" && comboBox2.Text == "A")
+            {
+                asignacion = 17;
+            }
+            else if (comboBox1.Text == "6" && comboBox2.Text == "B")
+            {
+                asignacion = 18;
+            }
+            else if (comboBox1.Text == "6" && comboBox2.Text == "C")
+            {
+                asignacion = 19;
+            }
+        }
+        private void MostrarTabla()
+        {
+            AsignacionID();
+
+            String CadenaConexion = "Server=localhost; User id=root; Database=boletas; Password=azr4510m;";
+            Conexion.ConnectionString = CadenaConexion;
+
+            MySqlCommand comandoconsulta = new MySqlCommand("select al.no_control, concat(al.apellidos,' ',al.nombre) as nombre, al.foto   " +
+                "from alumnos al  " +
+                "inner join asignacion a " +
+                "on al.id_asignacion = a.id_asignacion " +
+                "where al.id_asignacion = '" + asignacion + "';");
+
+            comandoconsulta.Connection = Conexion;
+            MySqlDataAdapter con = new MySqlDataAdapter(comandoconsulta);
+            DataSet Set = new DataSet();
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.RowTemplate.Height = 120;
+            dataGridView1.AllowUserToAddRows = false;
+
+            con.Fill(Set);
+
+            dataGridView1.DataSource = Set.Tables[0];
+
+            con.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
